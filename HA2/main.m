8e9f6b0dd49ecf07e2a1c875933cc7f1e646bba3
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2020-02-28 22:03:00 +0300

    Update main.m

--- HA2/main.m
+++ HA2/main.m
@@ -14,11 +14,6 @@ Ttool = eye(4)
 
 W = [900, 900, 900, 900, 900, 900]'
 
-n = 3
-
-dq = 2*pi
-dq = dq/exp
-
 phi = 0
 
 K1 = 1 / 1000000
@@ -27,18 +22,18 @@ K3 = 1 / 500000
 
 K = [K1, K2, K3]'
 
-traj = zeros(exp, n)
+traj = zeros(exp, 3)
 
-not_calibrated = zeros(exp, n)
+not_calibrated = zeros(exp, 3)
 
-calibrated = zeros(exp, n)
+calibrated = zeros(exp, 3)
 
 
-A = zeros(6, n)
+A = zeros(6, 3)
 
-B = zeros(n, n)
+B = zeros(3, 3)
 
-C = zeros(n, 1)
+C = zeros(3, 1)
 
 
 for i = 1:exp
@@ -48,29 +43,41 @@ for i = 1:exp
     newl2 = l2 + 0.23
     
     traj(i, :) = [newl2*cos(phi), newl2*sin(phi), newl1]
-    phi = phi + dq
+    phi = phi + 2*pi/exp
 end
 
+
+JT = Jt(Tbase, Ttool, q, t, [l1, l2])
+
 for i = 1:exp
-    JT = Jt(Tbase, Ttool, q, t, [l1, l2])
-    
-    for m = 1:n
-        A(:,m) = JT(:,m)*(JT(:,m)')*W
-    end
-    
     B = B + A' * A
     C = C + A' * A * K
+    for m = 1:3
+        A(:,m) = JT(:,m)*(JT(:,m)')*W
+    end
 end
+    
+B = B + A' * A
+C = C + A' * A * K
 
 newK = inv(B) * C
 
+a = 0
+b = 0
+c = 0
 
 for i = 1:exp
     dt = A * K + 0.0001*rand(6, 1)
-    not_calibrated(i,:) = [traj(i,1) + dt(1), traj(i,2) + dt(2), traj(i,3) + dt(3)]
+    a = dt(1) + traj(i,1)
+    b = dt(2) + traj(i,2)
+    c = dt(3) + traj(i,3)
+    not_calibrated(i,:) = [a, b, c]
     
     newdt = A * newK + 0.0001*rand(6, 1)
-    calibrated(i,:) = [traj(i,1) + newdt(1) - dt(1), traj(i,2) + newdt(2) - dt(2), traj(i,3) + newdt(3) - dt(3)]
+    a = newdt(1) + traj(i,1)
+    b = newdt(2) + traj(i,2)
+    c = newdt(3) + traj(i,3)
+    calibrated(i,:) = [a - dt(1), b - dt(2), c - dt(3)]
 end 
 
 traj = cat(1, traj(:,:), traj(1, :))
@@ -80,8 +87,10 @@ not_calibrated = cat(1, not_calibrated(:,:), not_calibrated(1, :))
 plot3(traj(:,1),traj(:,2),traj(:,3), 'o--')
 hold on
 grid on
+
 plot3(calibrated(:,1),calibrated(:,2),calibrated(:,3), 'o--')
 hold on
 grid on
+
 plot3(not_calibrated(:,1),not_calibrated(:,2),not_calibrated(:,3), 'o--')
 legend('target trajectory', 'with calibration', 'without calibration')
